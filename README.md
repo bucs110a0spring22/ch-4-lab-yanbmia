fbe30480b6311c988d0bd621622e332d831cfa61
Mia Yan <68654939+yanbmia@users.noreply.github.com>
2022-04-05 03:25:33 +0000

    update Midterm

--- README.md
+++ README.md
@@ -1,25 +1,28 @@
 #### CS 110
-# Chapter 4 - Lab - Functions
-
-### [Assignment Description](https://docs.google.com/document/d/1V20D_upUX4MO8YmskKlRB25Yu2pCEv3-h8z4EAfrSno/edit?usp=sharing)
+# Midterm - Python Programming
 
 ***
 
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
-Create sin, cos, and tan graph with a turtle.
-
-## GRACE DAYS
-Grace days used for this assignment: < 0 >
-
-Grace days remaining: < 5 >/5
+What did you clean up?:
+  Removed excess code in global space. All code runs from main().
+  Saved all values into distinct variable names.
+  
+Summary of function(s) added:
+  -> drawInverseSine(): creates an inverse sine graph (colored blue)
+  -> plotPoint(): allows user to add a point to the graph.
+  
+Summary of Feature Added:
+  Allows user to change background color, turtle shape, and adding units to the graph. 
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
-N/A
+Not sure if this is a bug. But when dart = dart.turtleTurtle() is in main, my code doesn't run. So I just added it to the top of main.py.
 
 ## REFERENCES:
-N/A
+To write out pi symbol: https://pythonforundergradengineers.com/unicode-characters-in-python.html
+
 
 ## MISCELLANEOUS COMMENTS:
 N/A
--- main.py
+++ main.py
@@ -1,100 +1,136 @@
 import turtle
-
-########### Your Code here ##############
-# You should only have functions here
-# If you have anything outside of a function, 
-# then you do not fully understand functions
-# and should review how they work or ask for help
-
 import math
 
-#y = (math.radians(270))
-#print(y)
+########### Your Code here ##############
+dart = turtle.Turtle()
 
-wn = turtle.Screen()
-fred = turtle.Turtle()
-#fred.goto(50,60)
-#fred.goto(-25,-30)
-#fred.goto(-45,50)
-#fred.goto(65,-75)
-#fred.clear()
+
+#additional feature
+windowColor = input("Which color for the background? ")
+turtleShape = input("Choose a pointer shape (arrow, turtle, circle): ")
+graphUnits = input("Units on the graph? (Type Yes or No): ")
+
 
 def setupWindow(mywindow=None):
   turtle.setworldcoordinates(-5,-5,5,5)
-  turtle.Screen().bgcolor("lavender")
+  turtle.Screen().bgcolor(windowColor)
+  dart.shape(turtleShape)
 
-setupWindow()
+#additional feature
+def setupUnits():
+  fred = turtle.Turtle()
+  fred.penup()
+  units = ["-2\u03C0", "-3\u03C0/2","-\u03C0", "-\u03C0/2", "0", "\u03C0/2", "\u03C0", "3\u03C0/2", "2\u03C0"]
+  count = 0
+  x_val = math.radians(-360)
+  for i in range(9):
+    fred.goto(x_val, -0.75)
+    fred.write(units[count])
+    count = count + 1
+    x_val = x_val + math.radians(90)
+  fred.hideturtle()
 
 def setupAxis(myturtle=None):
-  fred.goto(0,-5)
-  fred.goto(0,5)
-  fred.penup()
-  fred.goto(-10,0)
-  fred.pendown()
-  fred.goto(10,0)
-
-setupAxis()
-
+  dart.goto(0,-5)
+  dart.goto(0,5)
+  dart.penup()
+  dart.goto(-10,0)
+  dart.pendown()
+  dart.goto(10,0)
+  if graphUnits == "Yes":
+    setupUnits()
+    
 angles = range(-360,361)
 
-def drawSineCurve():
+def drawSineCurve(myturtle=None):
   for i in angles:
     y_val = math.sin(math.radians(i))
     x_val = math.radians(i)
-    fred.goto(x_val,y_val)
-  fred.pu()
+    dart.goto(x_val,y_val)
+  dart.pu()
     
-drawSineCurve()
-
 
 def drawCosineCurve(myturtle=None):
   for i in angles:
     y_val = math.cos(math.radians(i))
     x_val = math.radians(i)
-    fred.goto(x_val,y_val)
-    fred.pd()
-  fred.pu()
+    dart.goto(x_val,y_val)
+    dart.pd()
+  dart.pu()
 
-drawCosineCurve()
 
 def drawTangentCurve(myturtle=None):
   for i in angles:
     y_val = math.tan(math.radians(i))
     if y_val > 5:
       y_val = 5
-      fred.pu()
+      dart.pu()
     if y_val < -5:
       y_val = -5
-      fred.pu()
+      dart.pu()
     x_val = math.radians(i)
-    fred.goto(x_val,y_val)
-    fred.pd()
-  fred.pu()
+    dart.goto(x_val,y_val)
+    dart.pd()
+  dart.pu()
 
-drawTangentCurve()
+#new function
+def drawInverseSine(myturtle=None):
+  dart.color("blue")
+  for i in angles:
+    y_val = math.sin(math.radians(i))
+    if y_val == 0:
+      y_val = 5
+      dart.pu()
+    inverseY = 1/y_val
+    if inverseY > 5:
+      inverseY = 5
+      dart.pu()
+    if inverseY < -5:
+      inverseY = -5
+      dart.pu()
+    x_val = math.radians(i)
+    dart.goto(x_val,inverseY)
+    dart.pd()
+  dart.pu()
+
+value = input("Type a Coordinate [Format (x,y)]: ")
+def plotPoint(value):
+  albert = turtle.Turtle()
+  albert.color("red")
+  albert.width(4)
+  albert.penup()
+  x_val = int(value[1])
+  y_val = int(value[3])
+  point = (x_val,y_val)
+  albert.goto(point)
+  albert.write("X")
+  albert.hideturtle()
+  return(point)
+  
+
+def intersectionGraph():
+  
 
 
-wn.exitonclick()
+  
 
-
-
-
-
-##########  Do Not Alter Any Code Past Here ########
 def main():
-    #Part A
     wn = turtle.Screen()
     wn.tracer(5)
-    dart = turtle.Turtle()
+    #dart = turtle.Turtle()
     dart.speed(0)
     drawSineCurve(dart)
 
-    #Part B
     setupWindow(wn)
     setupAxis(dart)
     dart.speed(0)
+  
     drawSineCurve(dart)
     drawCosineCurve(dart)
     drawTangentCurve(dart)
+  
+    drawInverseSine(dart)
+    plotPoint(value)
     wn.exitonclick()
 main()
+
